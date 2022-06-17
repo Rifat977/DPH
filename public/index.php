115c2b36ab506bfba7c730932f6f5ed2cf928a57
rifat977 <itscrifat5147@gmail.com>
2022-06-18 01:37:22 +0600

    initial

--- public/index.php
+++ public/index.php
@@ -53,8 +53,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-
-if((!empty( $_SERVER['HTTP_X_FORWARDED_HOST'])) || (!empty( $_SERVER['HTTP_X_FORWARDED_FOR'])) ) {
- 	$_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
- 	$_SERVER['HTTPS'] = 'on';
- }
